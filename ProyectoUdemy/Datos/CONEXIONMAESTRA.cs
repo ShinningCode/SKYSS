cd5511c64356ce275c177748daf4e72fc4fbf8c4
unknown <a32997@uach.mx>
2024-01-22 12:08:58 -0600

    Conexion de datos

---
+++ ProyectoUdemy/Datos/CONEXIONMAESTRA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoUdemy.Datos
+{
+    public class CONEXIONMAESTRA
+    {
+        public static string conexion = @"Data source=DESKTOP-D6JQ9NG\SQLEXPRESS; Initial Catalog=SKYSS; Integrated Security=true";
+        public static SqlConnection conectar = new SqlConnection(conexion);
+        public static void abrir()
+        {
+            if (conectar.State == System.Data.ConnectionState.Closed) { 
+            conectar.Open();
+            }
+        }
+        public static void cerrar()
+        {
+            if (conectar.State == System.Data.ConnectionState.Open)
+            {
+                conectar.Close();
+            }
+        }
+    }
+}
---
+++ ProyectoUdemy/Datos/Dpersonal.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using ProyectoUdemy.Logica;
+using System.Windows.Forms;
+
+namespace ProyectoUdemy.Datos
+{
+    public class Dpersonal
+    {
+        public bool InsertarPersonal(Lpersonal parametros)
+        {
+			try
+			{
+                CONEXIONMAESTRA.abrir();
+                SqlCommand cmd = new SqlCommand("InsertarPersonal",CONEXIONMAESTRA.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Nombres", parametros.Nombres);
+                cmd.Parameters.AddWithValue("@Identificacion", parametros.Identificacion);
+                cmd.Parameters.AddWithValue("@Pais", parametros.Pais);
+                cmd.Parameters.AddWithValue("Id_cargo", parametros.Id_cargo);
+                cmd.Parameters.AddWithValue("SueldoPorHora", parametros.SueldoPorHora);
+                cmd.ExecuteNonQuery();
+                return true;
+
+
+            }
+			catch (Exception ex)
+			{
+                MessageBox.Show(ex.Message);
+                return false;
+			}
+            finally
+            {
+                CONEXIONMAESTRA.cerrar();
+            }
+        }
+        public bool EditarPersonal(Lpersonal parametros)
+        {
+            try
+            {
+                CONEXIONMAESTRA.abrir();
+                SqlCommand cmd = new SqlCommand("EditarPersonal", CONEXIONMAESTRA.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id_personal", parametros.Id_personal);
+                cmd.Parameters.AddWithValue("@Nombres", parametros.Nombres);
+                cmd.Parameters.AddWithValue("@Identificacion", parametros.Identificacion);
+                cmd.Parameters.AddWithValue("@Pais", parametros.Pais);
+                cmd.Parameters.AddWithValue("Id_cargo", parametros.Id_cargo);
+                cmd.Parameters.AddWithValue("SueldoPorHora", parametros.SueldoPorHora);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                CONEXIONMAESTRA.cerrar();
+            }
+        }
+        public bool EliminarPersonal(Lpersonal parametros)
+        {
+            try
+            {
+                CONEXIONMAESTRA.cerrar();
+                SqlCommand cmd = new SqlCommand("EliminarPersonal", CONEXIONMAESTRA.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Idpersonal", parametros.Id_personal);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                CONEXIONMAESTRA.cerrar();
+            }
+        }
+        public void MostrarPersonal(ref DataTable dt, int desde, int hasta)
+        {
+            try
+            {
+                CONEXIONMAESTRA.abrir();
+                SqlDataAdapter da = new SqlDataAdapter("MostrarPersonal", CONEXIONMAESTRA.conectar);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand.Parameters.AddWithValue("@Desde",desde);
+                da.SelectCommand.Parameters.AddWithValue("@Hasta", hasta);
+                da.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                CONEXIONMAESTRA.cerrar();
+            }
+        }
+        public void BuscarPersonal(ref DataTable dt, int desde, int hasta, string buscador)
+        {
+            try
+            {
+                CONEXIONMAESTRA.abrir();
+                SqlDataAdapter da = new SqlDataAdapter("BuscarPersonal", CONEXIONMAESTRA.conectar);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand.Parameters.AddWithValue("@Desde", desde);
+                da.SelectCommand.Parameters.AddWithValue("@Hasta", hasta);
+                da.SelectCommand.Parameters.AddWithValue("@Buscador", buscador);
+                da.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                CONEXIONMAESTRA.cerrar();
+            }
+        }
+
+    } 
+    }
+       
+}
